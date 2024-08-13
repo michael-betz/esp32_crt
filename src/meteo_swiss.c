dbfa18ab5037bff99697f9e58e963c8f68b91029
Michael Betz <michael.betz@terapet.ch>
2024-08-13 12:35:45 +0200

    3 day / 6h  weather forecast

--- src/meteo_swiss.c
+++ src/meteo_swiss.c
@@ -1,9 +1,11 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <time.h>
 #include <cjson/cJSON.h>
 #include "meteo_swiss.h"
 #include "fonts/font_data.h"
 
+// see dev/2022-02-14-Wetter-Icons-inkl-beschreibung-v1-an-website.xlsx
 static const uint8_t weather_icon_map_a[] = {
 	0x0d,  // 1
 	0x02,  // 2
@@ -114,19 +116,26 @@ static unsigned cp_from_meteo_swiss_key(unsigned key)
 
 int draw_weather_icons(cJSON *weather, unsigned h)
 {
+	char label[32];
+
 	cJSON *graph = cJSON_GetObjectItemCaseSensitive(weather, "graph");
 	if (graph == NULL) {
 		printf("graph not found\n");
 		return -1;
 	}
 
-	// cJSON *start = cJSON_GetObjectItemCaseSensitive(graph, "start");
-	// if (!cJSON_IsNumber(start)) {
-	// 	printf("start not found\n");
-	// 	return -1;
-	// }
-	// // start time is from midnight of the current day
-	// printf("start: %ld\n", (long)(start->valuedouble));
+	cJSON *start = cJSON_GetObjectItemCaseSensitive(graph, "start");
+	if (!cJSON_IsNumber(start)) {
+		printf("start not found\n");
+		return -1;
+	}
+	// start time is from midnight of the current day
+	time_t ts_start;
+	struct tm timeinfo;
+	// time(&ts_start);
+	// printf("time: %ld\n", ts_start);
+	ts_start = (long)(start->valuedouble) / 1000;
+	printf("start: %ld\n", ts_start);
 
 	cJSON *icons = cJSON_GetObjectItemCaseSensitive(graph, "weatherIcon3h");
 	if (icons == NULL) {
@@ -134,23 +143,50 @@ int draw_weather_icons(cJSON *weather, unsigned h)
 		return -1;
 	}
 
-	cJSON *icon = cJSON_GetArrayItem(icons, h);
-	if (!cJSON_IsNumber(icon))
-		return -1;
-
-	unsigned cp = cp_from_meteo_swiss_key(icon->valueint);
-	h *= 3;
-	int d = h / 24;
-	char label[32];
-	if (d > 0)
-		snprintf(label, sizeof(label), "+%d d %2d h", d, h % 24);
-	else
-		snprintf(label, sizeof(label), "%2d h", h);
-
-	printf("%s  icon: %3d, %04x\n", label, icon->valueint, cp);
-
+	// Draw 3 times x 3 hours of weather icons
 	set_font_name(&f_weather_icons);
-	push_char_at_pos(0, -500, cp, 1800, 100);
-	set_font_name(NULL);
-	push_str(0, -1500, label, sizeof(label), A_CENTER, 400, 100);
+	for (int day_ind=0; day_ind<=2; day_ind++) {
+		int x_pos = (day_ind - 1) * 750 + 300;
+
+		for (int h_ind=0; h_ind<=3; h_ind++) {
+			int y_pos = -((h_ind - 2) * 750 + 300);
+
+			// Draw hour labels
+			if (day_ind == 0) {
+				snprintf(label, sizeof(label), "%2d h", h_ind * 2 * 3);
+				set_font_name(NULL);
+				push_str(-1100, y_pos + 100, label, sizeof(label), A_CENTER, 300, 100);
+			}
+
+			int ind = day_ind * (24 / 3) + h_ind * 2;
+			cJSON *icon = cJSON_GetArrayItem(icons, ind);
+			if (!cJSON_IsNumber(icon))
+				return -1;
+
+			set_font_name(&f_weather_icons);
+			unsigned cp = cp_from_meteo_swiss_key(icon->valueint);
+			push_char_at_pos(x_pos, y_pos, cp, 500, 100);
+		}
+
+		// Draw weekday label
+		localtime_r(&ts_start, &timeinfo);
+    	strftime(label, sizeof(label), "%a", &timeinfo);
+		set_font_name(NULL);
+		push_str(x_pos, -1500, label, sizeof(label), A_CENTER, 300, 100);
+
+    	ts_start += 24 * 60 * 60;
+	}
+
+	// h *= 3;
+	// int d = h / 24;
+	// if (d > 0)
+	// 	snprintf(label, sizeof(label), "+%d d %2d h", d, h % 24);
+	// else
+	// 	snprintf(label, sizeof(label), "%2d h", h);
+
+	// printf("%s  icon: %3d, %04x\n", label, icon->valueint, cp);
+
+	// set_font_name(&f_weather_icons);
+	// push_char_at_pos(0, -500, cp, 1800, 100);
+	// push_str(0, -1500, label, sizeof(label), A_CENTER, 400, 100);
 }
